2f5959a89165155fd59837b6c76ee93e2394113d
VallesCris <cdvallesb7@gmail.com>
2022-08-02 14:06:21 -0500

    db ok

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    height:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    weight:{
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    years:{
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING
+    },
+    createInDb:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },{timestamps: false});
 };
